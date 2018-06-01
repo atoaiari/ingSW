ad839f0793a9e43a47c72c984e38d4fb64267245
atoaiari <atoaiari@gmail.com>
2018-06-01 12:03:52 +0200

    login error messages

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class LoginController {
 
@@ -18,6 +20,8 @@ public class LoginController {
     @FXML private Label forgetpswLabel;
     @FXML private Button loginButton;
     @FXML private Button registerButton;
+    @FXML private Label emailError;
+    @FXML private Label pswError;
 
     public LoginController(){
         //loginButton.setOnAction(e -> loginClick());
@@ -33,12 +37,42 @@ public class LoginController {
         this.loginStage = loginStage;
     }
 
-    private void loginClick() {
-        loginStage.close();
+    private String getMail(){
+        return emailTextfield.getText();
     }
 
-    public String getUsername(){
-        return emailTextfield.getText();
+    private String getPassword(){
+        return pswPasswordField.getText();
+    }
+
+    @FXML
+    public void loginClick() {
+        if (getMail() == null || getMail().length() == 0)
+            emailError.setText("Inserire email");
+        else if (getPassword() == null || getPassword().length() == 0)
+            pswError.setText("Inserisci password");
+        else {
+            logged=true;
+            loginStage.close();
+        }
+    }
+
+    @FXML
+    public void noErrorMail() {
+        if (getMail() != null || getMail().length() != 0)
+            emailError.setText("");
+    }
+
+    @FXML
+    public void noErrorPsw() {
+        if (getPassword() != null || getPassword().length() != 0)
+            pswError.setText("");
+    }
+
+    @FXML
+    public void keyboardEnterPressed(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER))
+            loginButton.fire();
     }
 
 }
